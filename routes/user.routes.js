e29ca1ea4423302c0cad71fa58e7897e3090fbe4
sachitmaster <sachitkumarsahu6@gmail.com>
2025-02-17 18:24:27 +0530

    update

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -22,4 +22,4 @@ router.post("/login", [
 router.get("/profile", authMiddleware.authUser, userController.getUserProfile)
 router.get("/logout", authMiddleware.authUser, userController.logoutUser)
 
-module.exports = router
+module.exports = route
